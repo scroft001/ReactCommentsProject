9e52ef61f478be1e89532c784a6302895a1488df
Steven Croft <scroft001@gmail.com>
2022-08-26 21:29:08 -0700

    Created new component

---
+++ comments/src/CommentDetail.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { faker } from '@faker-js/faker'
+
+const CommentDetail = () => {
+    return (
+            <div className='comment'> 
+                <a href='/' className='avatar'> 
+                    <img alt='avatar' src={faker.image.avatar()}/> 
+                </a> 
+                <div className='content'> 
+                    <a href='/' className='author'> Sam </a> 
+                    <div className='metaData'> 
+                        <span className='date'> Today at 6:00PM </span> 
+                    </div> 
+                    <div className='text'> Nice blog post! </div> 
+                </div> 
+            </div>
+    );
+};
--- comments/src/index.js
+++ comments/src/index.js
@@ -4,18 +4,7 @@ import { faker } from '@faker-js/faker'
 const App = () => { 
     return( 
         <div className='ui container comments'> 
-            <div className='comment'> 
-                <a href='/' className='avatar'> 
-                    <img alt='avatar' src={faker.image.avatar()}/> 
-                </a> 
-                <div className='content'> 
-                    <a href='/' className='author'> Sam </a> 
-                    <div className='metaData'> 
-                        <span className='date'> Today at 6:00PM </span> 
-                    </div> 
-                    <div className='text'> Nice blog post! </div> 
-                </div> 
-            </div>
+            
             <div className='comment'> 
                 <a href='/' className='avatar'> 
                     <img alt='avatar' src={faker.image.avatar()}/> 
